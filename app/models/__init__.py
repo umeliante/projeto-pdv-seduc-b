41e8ec76385e6b5373bc85c56c4ff8b9b8e87fa4
Danilo <xtdano007@gmail.com>
2026-08-19 14:40:12 -0300

    feat: add Cliente and Venda models with relationships and properties
    - Created Cliente model with fields for name, registration, phone, and association status.
    - Created Venda model with fields for client reference, user reference, discount percentage, gross and net totals, and observation.
    - Added ItemVenda model to handle sale items with relationships to Venda and Produto.
    - Implemented HTML templates for client management and sales processing, including forms for creating and editing clients, displaying client lists, and generating sales receipts.
    - Added migration scripts to create necessary database tables for clients and sales.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,9 +2,12 @@ from app.models import categoria
 from app.models import produto
 from app.models import usuarios
 from app.models import movimentacao
+from app.models import cliente
+from app.models import venda
+
 # Gerar a migration
 
-# python -m alembic revision --autogenerate -m "Criar tabela categorias e produtos."
+# python -m alembic revision --autogenerate -m "Criar tabela clientes e produtos."
 
 # aplicar a migration
 # python -m alembic upgrade head
