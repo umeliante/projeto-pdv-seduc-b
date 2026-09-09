f848686a574a5e346491791cecf0c13701b3edce
Danilo <xtdano007@gmail.com>
2026-09-09 16:07:34 -0300

    fix: correct variable name for product stock in low stock test

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@ from app.models.produto import Produto
 
 #Função para verificar o estoque baixo!
 def test_verificar_estoque_baixo_quando_estoque_e_zero():
-    produto1 = Produto(nome="Camisa P", estoque=0)
+    
+    produto1 = Produto(nome="Camisa P", estoque_atual=0)
 
     assert produto1.estoque_baixo is True
